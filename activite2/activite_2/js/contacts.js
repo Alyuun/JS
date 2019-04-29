98a7a1302f7cd18ec66d5c5c5c061bab0d294b9f
Alyun <alizeyun@gmail.com>
2019-04-29 14:49:09 +0200

    Do...while
    mise en place Do...while + if

--- activite2/activite_2/js/contacts.js
+++ activite2/activite_2/js/contacts.js
@@ -1,9 +1,22 @@
 /* 
-Activité : gestion des contacts
+Chaque contact a un nom et un prénom. Le programme gère un tableau de contacts et offre à l’utilisateur le choix entre les fonctionnalités suivantes :
+
+Afficher chaque contact du tableau.
+Quitter.
+Le programme doit “boucler” jusqu’à ce que l’utilisateur choisisse de quitter. Il doit également utiliser des objets pour gérer les contacts.
+
+Les deux contacts à insérer initialement dans le tableau sont :
+
+Carole Lévisse
+Mélodie Nelsonne
+Pour obtenir le maximum de points possibles, ajoutez la possibilité pour l’utilisateur de saisir un nouveau contact qui sera inséré dans le tableau.
+
+La vidéo ci-dessous permet d’observer le comportement du programme attendu.
 */
 
 // TODO : complétez le programme
-prompt("Choisissez une option : ")
+//console.log(prompt("Choisissez une option : "))
+console.log("Bienvenue dans le gestionnaire de contacts ! \n1 : Liste des contasts \n2 : Ajouter un nouveau contact \n0 : Quitter");
 
 class Contact 
 {
@@ -20,3 +33,47 @@ class Contact
 };
 
 
+do {
+	input = prompt("Choisissez une option : ");
+
+	if (input == 0)
+	{
+		console.log("Au revoir !");
+	} else if (input == 1)
+	{
+		console.log("Test 1");
+	} else if (input == 2)
+	{
+		console.log("Test 2")
+	} else
+	{
+		console.log("Veuillez saisir 0, 1 ou 2")
+	};
+}
+while (input != 0)
+
+
+/*do
+{
+	input = prompt("Choisissez une option : ");
+
+	switch (input)
+	{
+		case 0 :
+		alert(`"Au revoir !"`);
+		break;
+		case 1: 
+		alert(`"Test 1"`);
+		break;
+		case 2 :
+		alert(`"Test 2"`);
+		break;
+		default:
+		alert("Veuillez saisir 0, 1 ou 2");
+	}
+} while (input != 0)
+
+if(input == 0)
+{
+	break;
+}*/
